0f20047cef5b98fb8c1896c941fe3d5016b9a6ef
Armanimran07 <imrannajar05@gmail.com>
2026-06-16 21:06:37 +0800

    Fix local server basePath issue by making it dynamic

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/JK-Borewells",
+  basePath: repo !== "" ? repo : undefined,
   images: {
     unoptimized: true,
   },
